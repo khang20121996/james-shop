af013a579fb983025c339138e8b9c8145ced1546
Khang Nguyen <nguyentruongkhang.1509@gmail.com>
2023-03-16 18:54:00 +0700

    - Change API server public

--- js/api/axiosClient.js
+++ js/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:3000/api/",
+  baseURL: "https://james-shop-api.onrender.com/api/",
   headers: {
     "Content-Type": "application/json",
   },
